f3713ab0c09b9af2e96b8637e974ca6cca62b72e
DarkEllioto <adcn.smrld@gmail.com>
2023-06-03 14:06:09 +0200

    Portada

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -19,6 +19,7 @@ public class UIManager : Singleton<UIManager>
     [SerializeField] private GameObject panelInventario;
     [SerializeField] private GameObject panelMisiones;
     [SerializeField] private GameObject panelMisionesPersonaje;
+    [SerializeField] private GameObject panelInicio;
 
     //Creamos unas referencias para obtener la imagen de la vida,el mana, exp y el texto
 
@@ -143,6 +144,10 @@ public class UIManager : Singleton<UIManager>
     public void abrirCerrarPersonajeMisiones()
     {
         panelMisionesPersonaje.SetActive(!panelMisionesPersonaje.activeSelf);
+    }
+    public void abrirCerrarInicio()
+    {
+        panelInicio.SetActive(!panelInicio.activeSelf);
     }
     //Creamos un metodo para actualizar la exp actual del personaje
     public void ActualizarExpPersonaje(float pExpAc, float pExpReq)
